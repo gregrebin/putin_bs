a1f3943f6f2206adfe90c71adbeb1f611c870ca5
gregrebin <me@gregrebin.com>
2023-12-20 17:23:40 +0100

    Remove parsed links from list

--- content.py
+++ content.py
@@ -2,15 +2,16 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-if __name__ == '__main__':
-    file = open("links_short.txt", "r")
+
+def main(filename):
+    file = open(filename, "r")
     links = file.readlines()
     file.close()
     driver = webdriver.Chrome()
     driver.implicitly_wait(3)
     i = 1
     total = len(links)
-    for link in links:
+    for link in links[:]:
         print(link)
         print(str(i) + "/" + str(total))
         driver.get(link)
@@ -19,9 +20,15 @@ if __name__ == '__main__':
             date = driver.find_element(By.CLASS_NAME, "read__published").get_attribute("datetime")
             with open("content/" + date + ".txt", "w") as file:
                 file.write(text)
+            links.remove(link)
         except Exception:
-            with open("missing.txt", "a") as file:
-                file.write(link)
+            with open(filename, "w") as file:
+                for remaining in links:
+                    file.write(remaining)
+            return
         i += 1
-        time.sleep(3)
+        time.sleep(1)
 
+
+if __name__ == '__main__':
+    main("links1.txt")
